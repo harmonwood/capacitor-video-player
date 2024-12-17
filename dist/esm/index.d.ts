c60bd4fad485c1db2132b303c3ec9eb528af9ac2
Harmon Wood <harmon@harmonwood.com>
2024-12-17 15:56:16 -0600

    chore(): removing dist from git repo

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { CapacitorVideoPlayerPlugin } from './definitions';
-declare const CapacitorVideoPlayer: CapacitorVideoPlayerPlugin;
-export * from './definitions';
-export { CapacitorVideoPlayer };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const CapacitorVideoPlayer = registerPlugin('CapacitorVideoPlayer', {
-    web: () => import('./web').then(m => new m.CapacitorVideoPlayerWeb()),
-});
-export * from './definitions';
-export { CapacitorVideoPlayer };
-//# sourceMappingURL=index.js.map
